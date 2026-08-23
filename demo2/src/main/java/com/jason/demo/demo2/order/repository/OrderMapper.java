0b7135cb622abb435206bd7c1f3d0e888a597bf5
jasonsoso <648636045@qq.com>
2026-08-24 07:31:18 +0800

    feat(demo2): implement Snowflake ID generation configuration
    - Added SnowflakeIdConfiguration for allocating node IDs via Redis and generating unique IDs.
    - Introduced SnowflakeProperties for configuration settings related to Snowflake ID generation.
    - Removed deprecated OrderEntity, OrderMapper, and OrderRepository classes to streamline the codebase.

--- demo2/src/main/java/com/jason/demo/demo2/order/repository/OrderMapper.java
+++
@@ -1,8 +0,0 @@
-package com.jason.demo.demo2.order.repository;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OrderMapper extends BaseMapper<OrderEntity> {
-}
